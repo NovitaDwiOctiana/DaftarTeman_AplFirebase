9c1bbbd3c0eadc41436be08f0bace0075fc7c7ba
nvitaadd <novitadwioctaviana@gmail.com>
2021-06-04 12:51:45 +0700

    Daftar_Teman tugas prak 7

--- app/src/main/java/com/example/daftarteman/MainActivity.kt
+++ app/src/main/java/com/example/daftarteman/MainActivity.kt
@@ -76,7 +76,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                         })
             }
             R.id.show_data -> {
-
+                startActivity(Intent(this@MainActivity, MyListData::class.java))
             }
         }
     }}
